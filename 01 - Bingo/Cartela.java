b64933393d049a2ccd8b0e7e9175e6da30d39c2a
Miichael <105318393+Maike995@users.noreply.github.com>
2022-09-26 00:30:21 +0000

    Consegue fazer com que n se repetisse e que ficasse em ordem crescente

--- 01 - Bingo/Cartela.java
+++ 01 - Bingo/Cartela.java
@@ -1,33 +1,95 @@
-
 import java.util.Random;
 import java.util.ArrayList;
 
 public class Cartela {
-
-    int[][] cartelaGerada = new int[5][5];
-
-  ArrayList salvarCartelas = new ArrayList();
   
+  int[][] cartelaGerada = new int[6][6];
+  Random gerador = new Random();
+  ArrayList salvarCartelas = new ArrayList();
+    
     public Cartela(){
-        for(int i = 0; i<5; i++){
-          for(int z = 0; z<5; z++){
 
-                    Random gerador = new Random();
-                    cartelaGerada [i][z] = gerador.nextInt(60)+1;
+      for(int i = 0; i<5; i++){
+        for(int z = 0; z<5; z++){
 
-                }
-            }
+           cartelaGerada [i][z] = gerador.nextInt(60)+1;
+        } 
+        cartelaGerada [i][5] = gerador.nextInt(60)+1;
+      }
       salvarCartelas.add(cartelaGerada);
     }
 
-  public void imprimirCartela(){
+    public void imprimirCartela(){
       for(int v = 0; v<5; v++){
         for(int b = 0; b<5; b++){
-     System.out.print(cartelaGerada[v][b] + " ");
-        }System.out.println();
+          System.out.print(cartelaGerada[v][b] + " ");
+        }
+        System.out.println();
       }
-  }
-
-}
+    }
+  
+    public void ordem(){
     
-   
+      int[][] organizador = new int[1][1];
+      
+        for(int c = 0; c<100000; c++){
+          for(int b = 0; b<5; b++){
+            for(int a = 0; a<5; a++){
+              if(cartelaGerada[b][a]>cartelaGerada[b][a+1]){
+                organizador   [0][0]   = cartelaGerada[b][a];
+                cartelaGerada [b][a]   = cartelaGerada[b][a+1];
+                cartelaGerada [b][a+1] = organizador[0][0];
+              }
+              if(cartelaGerada[b][4]>cartelaGerada[b+1][0]){
+                organizador   [0][0]   = cartelaGerada[b][4];
+                cartelaGerada [b][4]   = cartelaGerada[b+1][0];
+                cartelaGerada [b+1][0] = organizador[0][0];
+              }
+              if(cartelaGerada[b][a]==cartelaGerada[b][a+1]){
+                organizador   [0][0]   = gerador.nextInt(60)+1;
+                cartelaGerada [b][a]   = cartelaGerada[b][a+1];
+                cartelaGerada [b][a+1] = organizador[0][0];
+              }
+              if(cartelaGerada[b][4]==cartelaGerada[b+1][0]){
+                organizador   [0][0]   = gerador.nextInt(60)+1;
+                cartelaGerada [b][4]   = cartelaGerada[b+1][0];
+                cartelaGerada [b+1][0] = organizador[0][0];
+              }
+            }
+          }
+        
+          for(int x = 0; x<5; x++){
+            for(int z = 0; z<5; z++){
+              if(cartelaGerada[x][z]==cartelaGerada[x][z+1]){
+                if(cartelaGerada[x][z]<30){
+                  cartelaGerada[x][z]=cartelaGerada[x][z]+1;
+                }else{
+                  cartelaGerada[x][z]=cartelaGerada[x][z]-1;
+                }
+              }
+              if(cartelaGerada[x][z]==cartelaGerada[x+1][z]){
+                if(cartelaGerada[x][z]<30){
+                  cartelaGerada[x][z]=cartelaGerada[x][z]+1;
+                }else{
+                  cartelaGerada[x][z]=cartelaGerada[x][z]-1;
+                }
+                if(cartelaGerada[x][z]==cartelaGerada[x+1][0]){
+                  if(cartelaGerada[x][z]<30){
+                    cartelaGerada[x][z]=cartelaGerada[x+1][0]+1;
+                  }else{
+                    cartelaGerada[x][z]=cartelaGerada[x+1][0]-1;
+                  }
+                }
+              }
+            }      
+          }
+        }
+
+    }
+             
+}
+                
+
+  
+  
+
--- 01 - Bingo/Main.java
+++ 01 - Bingo/Main.java
@@ -1,21 +1,14 @@
-
-
-
 public class Main {
 
-   
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-
-      for(int q=1;q<11;q++){
-        System.out.println();
-        System.out.println("Cartela "+q );
-      Cartela gerar = new Cartela();
+    for(int q=1;q<11;q++){
+      System.out.println();
+      System.out.println("Cartela "+q );
+    Cartela gerar = new Cartela();
+      gerar.ordem();
       gerar.imprimirCartela();
-       
-      }
-       
+      
     }
-
+  }
 }
-
